d038c540e48f13b2a350dbcd8d11f1f95c95cb31
Thomas Maroschik <tmaroschik@dfau.de>
2019-07-19 12:01:49 +0200

    [TASK] Api middleware should always run after auth middleware

--- Configuration/RequestMiddlewares.php
+++ Configuration/RequestMiddlewares.php
@@ -4,13 +4,17 @@ return [
     'frontend' => [
         'dfau/toujou-api/api-entrypoint' => [
             'target' => \DFAU\ToujouApi\Middleware\ApiEntrypoint::class,
-            'after' => ['typo3/cms-frontend/site', 'dfau/toujou-oauth2-server/authorization-server'],
+            'after' => ['typo3/cms-frontend/site', 'dfau/toujou-oauth2-server/resource-server'],
             'before' => ['typo3/cms-frontend/base-redirect-resolver']
         ],
     ],
     'toujou_api' => [
+        'middlewares/payload/json-payload' => [
+            'target' => \Middlewares\JsonPayload::class
+        ],
         'dfau/toujou-api/router' => [
-            'target' => \DFAU\ToujouApi\Middleware\Router::class
+            'target' => \DFAU\ToujouApi\Middleware\Router::class,
+            'after' => ['middlewares/payload/json-payload']
         ]
     ]
 ];
