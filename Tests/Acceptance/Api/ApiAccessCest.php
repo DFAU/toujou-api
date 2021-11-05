a86d2b3794ec2ec49c81ba9716beecb1100bc0b7
Tobias Wojtylak <twojtylak@dfau.de>
2021-11-05 17:47:33 +0100

    tests: acceptance test for api access

--- Tests/Acceptance/Api/ApiAccessCest.php
+++ Tests/Acceptance/Api/ApiAccessCest.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace DFAU\ToujouApi\Tests\Acceptance\Api;
@@ -8,13 +9,35 @@ use DFAU\ToujouApi\Tests\Acceptance\Support\ApiTester;
 
 class ApiAccessCest
 {
-
-    /**
-     * @param ApiTester $I
-     */
-    public function testUnauthorizedAccess(ApiTester $I)
+    public function testUnauthorizedAccess(ApiTester $I): void
     {
+        $I->markTestSkipped('todo: register api routes');
+
         $I->sendGET('pages');
         $I->seeResponseCodeIs(HttpCode::UNAUTHORIZED); // 401
+    }
+
+    public function testGetAccessTokenWithInvalidCredentials(ApiTester $I): void
+    {
+        $data = \json_encode([
+            'grant_type' => 'client_credentials',
+            'client_id' => 'invalid',
+            'client_secret' => 'invalid',
+        ], JSON_THROW_ON_ERROR);
+
+        $I->sendPOST('token', $data);
+        $I->seeResponseCodeIs(HttpCode::UNAUTHORIZED); // 401
+    }
+
+    public function testGetAccessTokenWithValidCredentials(ApiTester $I): void
+    {
+        $data = \json_encode([
+            'grant_type' => 'client_credentials',
+            'client_id' => '1234',
+            'client_secret' => '567',
+        ], JSON_THROW_ON_ERROR);
+
+        $I->sendPOST('token', $data);
+        $I->seeResponseCodeIs(HttpCode::OK); // 200
     }
 }
--- Tests/Acceptance/Support/ApiTester.php
+++ Tests/Acceptance/Support/ApiTester.php
@@ -1,16 +1,17 @@
 <?php
 
 declare(strict_types=1);
+
 namespace DFAU\ToujouApi\Tests\Acceptance\Support;
 
-use DFAU\ToujouApi\Tests\Acceptance\Support\_generated\BackendTesterActions;
+use DFAU\ToujouApi\Tests\Acceptance\Support\_generated\ApiTesterActions;
 use TYPO3\TestingFramework\Core\Acceptance\Step\FrameSteps;
 
 /**
  * Default backend admin or editor actor in the backend
-*/
+ */
 class ApiTester extends \Codeception\Actor
 {
-    use BackendTesterActions;
+    use ApiTesterActions;
     use FrameSteps;
 }
