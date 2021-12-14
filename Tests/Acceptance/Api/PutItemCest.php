d8b2f223a9196d895d981d5f222fdf0d9114399a
Tobias Wojtylak <twojtylak@dfau.de>
2021-12-14 09:21:18 +0100

    tests: add test for creating items via put request

--- Tests/Acceptance/Api/PutItemCest.php
+++ Tests/Acceptance/Api/PutItemCest.php
@@ -11,11 +11,45 @@ class PutItemCest
 {
     public function testUpdateItem(ApiTester $I): void
     {
-        $data = '{"data":{"type":"pages","id":"1","attributes":{"pid":0,"doktype":1,"title":"Hello World","slug":null,"nav_title":"","subtitle":"","abstract":null,"keywords":null,"description":null,"author":"","author_email":"","lastUpdated":0,"layout":0,"newUntil":0,"backend_layout":"","backend_layout_next_level":"","content_from_pid":0,"target":"","cache_timeout":0,"cache_tags":"","is_siteroot":1,"no_search":0,"php_tree_stop":0,"module":"","media":0,"tsconfig_includes":null,"TSconfig":null,"l18n_cfg":0,"hidden":0,"nav_hide":0,"starttime":0,"endtime":0,"extendToSubpages":0,"fe_group":"0","fe_login_mode":0,"editlock":0,"categories":0,"rowDescription":null},"meta":{"uid":1}}}';
+        $data = [
+            'data' => [
+                'type' => 'pages',
+                'id' => '1',
+                'attributes' => ['title' => 'Hello World'],
+            ],
+        ];
 
         $I->wantToBeBearerAuthenticated();
         $I->haveHttpHeader('Content-Type', 'application/vnd.api+json');
-        $I->sendPut('pages/1', $data);
+        $I->sendPut('pages/1?fields[pages]=title', $data);
+        $I->seeResponseCodeIs(HttpCode::ACCEPTED);
+
+        $I->sendGet('pages/1?fields[pages]=title');
+
+        $I->seeResponseContainsJson([
+            'data' => [
+                'attributes' => [
+                    'title' => 'Hello World',
+                ],
+            ],
+        ]);
+    }
+
+    public function testCreateItem(ApiTester $I): void
+    {
+        $data = [
+            'data' => [
+                'type' => 'pages',
+                'attributes' => [
+                    'title' => 'Hello World',
+                    'pid' => 1,
+                ],
+            ],
+        ];
+
+        $I->wantToBeBearerAuthenticated();
+        $I->haveHttpHeader('Content-Type', 'application/vnd.api+json');
+        $I->sendPut('pages/1234?fields[pages]=title,pid', $data);
         $I->seeResponseCodeIs(HttpCode::ACCEPTED);
     }
 
