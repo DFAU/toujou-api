633f0a699c5e14d9acc5d9e7a9d967401484c60e
Tobias Wojtylak <twojtylak@dfau.de>
2021-12-20 16:41:43 +0100

    fix: let FileTransformer implement ResourceTransformerInterface

--- Classes/Domain/Transformer/FileTransformer.php
+++ Classes/Domain/Transformer/FileTransformer.php
@@ -5,21 +5,22 @@ declare(strict_types=1);
 namespace DFAU\ToujouApi\Domain\Transformer;
 
 use DFAU\ToujouApi\Domain\Repository\FileRepository;
+use DFAU\ToujouApi\Transformer\ResourceTransformerInterface;
 use League\Fractal\TransformerAbstract;
 
-class FileTransformer extends TransformerAbstract
+class FileTransformer extends TransformerAbstract implements ResourceTransformerInterface
 {
-    public function transform(array $file): array
+    public function transform($data): array
     {
         return [
-            'id' => (string) $file[FileRepository::DEFAULT_IDENTIFIER],
-            'meta' => $file[FileRepository::META_ATTRIBUTE],
-            'file_type' => $file['type'],
-            'size' => $file['size'],
-            'extension' => $file['extension'],
-            'mime_type' => $file['mime_type'],
-            'checksum' => $file['checksum'],
-            'sha1' => $file['sha1'],
+            'id' => (string) $data[FileRepository::DEFAULT_IDENTIFIER],
+            'meta' => $data[FileRepository::META_ATTRIBUTE],
+            'file_type' => $data['type'],
+            'size' => $data['size'],
+            'extension' => $data['extension'],
+            'mime_type' => $data['mime_type'],
+            'checksum' => $data['checksum'],
+            'sha1' => $data['sha1'],
         ];
     }
 }
